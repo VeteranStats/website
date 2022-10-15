50f6cb3f9475b86502afc6979f03294db4e6b017
Not TCA <realtcatech@gmail.com>
2022-10-15 21:12:57 +0800

    add some basic content + change bg color and font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="h-screen flex flex-col items-center text-center pt-5">
+      <h1 className="font-extrabold text-5xl">Great things are coming...</h1>
+      <p className="mt-2 max-w-2xl">
+        We're working really hard to help the{" "}
+        <span className="text-yellow-300">stats community.</span> In the
+        meantime, you can fill in the form below to get notified when we blast
+        off.
+      </p>
+    </div>
+  );
 }
