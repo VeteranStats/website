b9dfe428bb991fcf7efc51a2e5743c509371da74
Not TCA <realtcatech@gmail.com>
2022-10-15 21:14:26 +0800

    fix no unescaped entities errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="h-screen flex flex-col items-center text-center pt-5">
       <h1 className="font-extrabold text-5xl">Great things are coming...</h1>
       <p className="mt-2 max-w-2xl">
-        We're working really hard to help the{" "}
+        We&apos;re working really hard to help the{" "}
         <span className="text-yellow-300">stats community.</span> In the
         meantime, you can fill in the form below to get notified when we blast
         off.
