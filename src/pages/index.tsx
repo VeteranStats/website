71f1426d71b7c1272380f19f0e3dad182d4901d6
Not TCA <realtcatech@gmail.com>
2022-10-15 21:31:46 +0800

    move everything to the center

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col items-center text-center pt-5">
+    <div className="h-screen flex flex-col justify-center items-center text-center">
       <h1 className="font-extrabold text-5xl">Great things are coming...</h1>
       <p className="mt-2 max-w-xl">
         We&apos;re working really hard to help the{" "}
